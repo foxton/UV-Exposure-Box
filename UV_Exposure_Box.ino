204f64d6d25b2c307e510018bf5c074654fed8ec
foxton <foxx89@hotmail.com>
2013-10-04 19:15:13 +0200

    V0.15

--- UV_Exposure_Box.ino
+++ UV_Exposure_Box.ino
@@ -6,19 +6,34 @@ int motorPin1 = 8; //Brown
 int motorPin2 = 9; //Yellow
 int motorPin3 = 10;//Black
 int motorPin4 = 11;//Orange
-int delayTime = 10;
-
+int switchpin1 = 2;
+int pinstate1 = 0;
+int delayTime = 10; // This delay will adjust the motor speed (Lower = faster)
+int ledPin =  13;  //Onboard LED for indicating the state of switchpin1
 void setup() 
 {
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
+  pinMode(switchpin1, INPUT);
+  pinMode(ledPin, OUTPUT);   
 }
 
 void loop() 
 {
-CCRotation();
+ pinstate1 = digitalRead(switchpin1); //Reads the state of the switch
+ if(pinstate1 == LOW)
+ {
+  CRotation();						  //Clockwise rotation
+   digitalWrite(ledPin, LOW);
+ }
+ else
+ {
+ CCRotation();						  //Counter clockwise rotation
+ digitalWrite(ledPin, HIGH);
+
+ }
 }
 
 void CRotation()
