d00d128fc6305f5f841e717c05abd2cace51784f
foxton <foxx89@hotmail.com>
2013-10-05 13:12:32 +0200

    V0.15

---
+++ Old versions/V0.15/UV_Exposure_Box.ino
@@ -0,0 +1,91 @@
+
+//Program for running a mototech unipolar 5-wire stepper motor
+//With a ULN2003 transistor array.
+
+int motorPin1 = 8; //Brown
+int motorPin2 = 9; //Yellow
+int motorPin3 = 10;//Black
+int motorPin4 = 11;//Orange
+int switchpin1 = 2;
+int pinstate1 = 0;    // Stores the state of the switch
+int oldpinstate1 = 0; //Previous state of the switch
+int delayTime = 10; // This delay will adjust the motor speed (Lower = faster)
+int ledPin =  13;  //Onboard LED for indicating the state of switchpin1
+void setup() 
+{
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  pinMode(switchpin1, INPUT);
+  pinMode(ledPin, OUTPUT);   
+}
+
+void loop() 
+{
+ pinstate1 = digitalRead(switchpin1); //Reads the state of the switch
+ if(pinstate1 == LOW)
+ {
+  CRotation();						  //Clockwise rotation
+  
+   digitalWrite(ledPin, LOW);
+   pinstate1 = oldpinstate1;
+   pinstate1 = digitalRead(switchpin1);
+ }
+ else
+ {
+ CCRotation();						  //Counter clockwise rotation
+ pinstate1 = oldpinstate1;
+ digitalWrite(ledPin, HIGH);
+ pinstate1 = digitalRead(switchpin1);
+ 
+
+ }
+}
+
+void CRotation()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(delayTime);
+}
+void CCRotation()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+}
